84e73db0eab582b67be82dee977b905a054dd638
Aneesh-382005 <aneesh382005@gmail.com>
2025-06-13 21:54:51 +0530

    Get YouTube Transcript

---
+++ backend/app/services/transcript.py
@@ -0,0 +1,35 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
+
+class GETTranscript:
+    def __init__(self, url):
+        self.url = url
+        self.videoID = self.parseURL(url)
+        if not self.videoID:
+            raise ValueError("Invalid YouTube URL")
+    
+    def parseURL(url):
+        pattern = (
+            r'(?:https?:\/\/)?(?:www\.)?'
+            r'(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/|v\/|.+\?v=)?'
+            r'([^#&?\/]{11})'
+        )
+        match = re.search(pattern, url)
+        if match:
+            return match.group(1)
+        else:
+            return None
+    
+    def fetchTranscript(self):
+        YTTAPI = YouTubeTranscriptApi()
+        fetchedTranscript = YTTAPI.fetch(self.videoID)
+        return fetchedTranscript
+    
+    def getRawData(self):
+        fetchedTranscript = self.fetchTranscript()
+        return fetchedTranscript.to_raw_data()
+    
+    def getTranscriptText(self):
+        fetchedTranscript = self.fetchTranscript()
+        for snippet in fetchedTranscript:
+            print(snippet.text)
